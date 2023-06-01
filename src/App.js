48ea558fc49bd31208155dc75150b256465a98fa
Srinivas A <56465971+battletremor@users.noreply.github.com>
2023-06-01 12:32:51 +0530

    12:32

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+
+
+
+const App = () => {
+
+  const [name, setName] = useState("");
+
+
+
+
+  const handleChange = (event) => {
+
+    setName(event.target.value);
+
+  };
+
+
+
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    console.log({ name });
+
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="name">Name</label>
+
+        <input id="name" type="text" value={name} onChange={handleChange} />
+
+        <button type="submit">Submit</button>
+
+      </form>
+
     </div>
+
   );
-}
+
+};
+
+
+
 
 export default App;
-
-//commit test
-//commit test 2
-//commit test 3
